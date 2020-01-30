024eab32c06ae96d333990b74362da0b6fb37472
amina-boukhris-sonarsource <51891356+amina-boukhris-sonarsource@users.noreply.github.com>
2020-01-30 13:54:29 +0100

    Update sonartest.js

--- com.sonarsource.js.test/sonartest.js
+++ com.sonarsource.js.test/sonartest.js
@@ -2,3 +2,8 @@ doument.writel("I am only made for test")
 var merged = arr.reduce(function(a, b) {
   a.concat(b);
 }); // Noncompliant: No return statement
+function say(a, b) {
+  print(a + " " + b);
+}
+
+say("hello", "world", "!"); // Noncompliant; last argument is not used
