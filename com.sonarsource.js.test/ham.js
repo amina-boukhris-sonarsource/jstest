5c8c4c33314a405a6f5af8928d0f7edc66087f14
amina-boukhris-sonarsource <51891356+amina-boukhris-sonarsource@users.noreply.github.com>
2020-01-30 15:55:45 +0100

    Create ham.js
    let

---
+++ com.sonarsource.js.test/ham.js
@@ -0,0 +1,14 @@
+say("hello", "world", "!"); // Noncompliant; last argument is not used
+ fruits[1] = "banana";
+ fruits[1] = "apple";  // Noncompliant - value on index 1 is overwritten
+
+ myMap.set("key", 1);
+ myMap.set("key", 2); // Noncompliant - value for key "key" is replaced
+
+ mySet.add(1);
+ mySet.add(1); // Noncompliant - element is already in the set
+var Person = Backbone.Model.extend({
+    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
+        favoriteColors: ["blue","purple","raspberry"]
+    }
+});
