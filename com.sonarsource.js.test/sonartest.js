f463242d393feb623c1dbe8d4551fb3aadf56956
amina-boukhris-sonarsource <51891356+amina-boukhris-sonarsource@users.noreply.github.com>
2020-01-30 14:45:53 +0100

    Update sonartest.js

--- com.sonarsource.js.test/sonartest.js
+++ com.sonarsource.js.test/sonartest.js
@@ -7,3 +7,11 @@ function say(a, b) {
 }
 
 say("hello", "world", "!"); // Noncompliant; last argument is not used
+ fruits[1] = "banana";
+ fruits[1] = "apple";  // Noncompliant - value on index 1 is overwritten
+
+ myMap.set("key", 1);
+ myMap.set("key", 2); // Noncompliant - value for key "key" is replaced
+
+ mySet.add(1);
+ mySet.add(1); // Noncompliant - element is already in the set
