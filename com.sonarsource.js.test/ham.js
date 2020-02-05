023e3bb03d16bd037f8d7895e422f92909188ef6
amina-boukhris-sonarsource <51891356+amina-boukhris-sonarsource@users.noreply.github.com>
2020-02-05 15:59:04 +0100

    Update ham.js
    phase

--- com.sonarsource.js.test/ham.js
+++ com.sonarsource.js.test/ham.js
@@ -12,23 +12,3 @@ var Person = Backbone.Model.extend({
         favoriteColors: ["blue","purple","raspberry"]
     }
 });
- mySet1.add(8);
- mySet1.add(7); // Noncompliant - element is already in the set
-var Person = Backbone.Model.extend({
-    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
-        favoriteColors: ["blue","purple","raspberry"]
-    }
-});
-
-x = 42;
-y = x.length;   // Noncompliant, Number type doesn't have "length" property
- mySet1.add(8);
- mySet1.add(7); // Noncompliant - element is already in the set
-var Person = Backbone.Model.extend({
-    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
-        favoriteColors: ["blue","purple","raspberry"]
-    }
-});
-say("hello", "world", "!"); // Noncompliant; last argument is not used
- fruits[1] = "banana";
- fruits[1] = "apple";  // Noncompliant - value on index 1 is overwritten
