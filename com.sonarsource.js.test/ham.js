fe28635c13c18f092e0c6978b3759d9d18dee629
amina-boukhris-sonarsource <51891356+amina-boukhris-sonarsource@users.noreply.github.com>
2020-02-05 15:36:50 +0100

    test build jenkins
    it should assign automatically new issues

--- com.sonarsource.js.test/ham.js
+++ com.sonarsource.js.test/ham.js
@@ -22,3 +22,10 @@ var Person = Backbone.Model.extend({
 
 x = 42;
 y = x.length;   // Noncompliant, Number type doesn't have "length" property
+ mySet1.add(8);
+ mySet1.add(7); // Noncompliant - element is already in the set
+var Person = Backbone.Model.extend({
+    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
+        favoriteColors: ["blue","purple","raspberry"]
+    }
+});
